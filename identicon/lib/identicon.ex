072e21bca600dc384ee77b875247dc24cee6667e
ricardo <linhares87@gmail.com>
2019-08-03 18:12:35 -0300

    Added coments on main function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,5 +1,21 @@
 defmodule Identicon do
 
+  @moduledoc """
+  
+  """
+
+  @doc """
+    Function for create a hash of input
+
+    ## Parameters
+
+      - input: string that representes the value to be hashed.
+
+    ## Example
+      iex(1)> Identicon.main("orange")
+      [254, 1, 214, 122, 0, 45, 250, 15, 58, 192, 132, 41, 129, 66, 236, 205]
+
+  """
   def main(input) do
     input
     |> hash_input
