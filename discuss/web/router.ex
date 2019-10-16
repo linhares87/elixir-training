13a2576233b96786e03ef196ff4ebafbd89b4763
ricardo <linhares87@gmail.com>
2019-10-15 21:03:49 -0300

    Added the update on the route for editing topics

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -20,6 +20,7 @@ defmodule Discuss.Router do
      get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
      get "/topics/:id/edit", TopicController, :edit
+     put "/topics/:id/update", TopicController, :update
 
   end
 
