38de8b7c923818f845c8de2389bdd66c132ef7db
Ricardo Linhares <linhares.ricardo@outlook.com>
2019-12-16 21:05:40 -0300

    authentication with git.

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -4,11 +4,24 @@ defmodule Discuss.AuthController do
 
   alias Discuss.User
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = _conn, _params) do
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
     changeset = User.changeset(%User{}, user_params)
-    merge_user(changeset)
+    signin(conn, changeset)
+  end
 
+  defp signin(conn, changeset) do
+    case merge_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:info, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 
   defp merge_user(changeset) do #insert_or_update_user
